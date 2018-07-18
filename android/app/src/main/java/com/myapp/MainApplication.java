fbf2835431be48d9990ba3c681f7326eeadbb173
liujiao <slience9@163.com>
2018-07-18 21:27:08 +0800

    21312

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -1,6 +1,10 @@
 package com.myapp;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -8,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.controllers.ActivityCallbacks;
 
 import java.util.Arrays;
 import java.util.List;
@@ -46,7 +51,85 @@ import java.util.List;
 }*/
 public class MainApplication extends NavigationApplication {
 
-  @Override
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setActivityCallbacks(new ActivityCallbacks(){
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                super.onActivityCreated(activity, savedInstanceState);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                super.onActivityStarted(activity);
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                super.onActivityResumed(activity);
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                super.onActivityPaused(activity);
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                super.onActivityStopped(activity);
+            }
+
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                super.onActivityDestroyed(activity);
+            }
+        });
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+
+        });
+    }
+
+    @Override
   public boolean isDebug() {
     // Make sure you are using BuildConfig from your own application
     return BuildConfig.DEBUG;
--- testnavigation.js
+++ testnavigation.js
@@ -20,41 +20,58 @@ registerScreens(); // this is where you register all of your app's screens
 
 // })
 
-
-Navigation.startTabBasedApp({
-    tabs: [
-        {
-            label: 'One',
-            screen: 'navigation.FirstTabScreen', // this is a registered name for a screen
-            icon: require('./img/select.png'),
-            selectedIcon: require('./img/unselect.png'), // iOS only
-            title: 'Screen One',
-            titleImage: require('./img/select.png'),
-            navigatorStyle: {
-
-            },
-            navigatorButtons: {}
-        },
-        {
-            label: 'Two',
-            screen: 'navigation.SecondTabScreen',
-            icon: require('./img/select.png'),
-            selectedIcon: require('./img/unselect.png'), // iOS only
-            title: 'Screen Two'
-        }
-    ],
-    tabsStyle: {
-        tabBarButtonColor: '#985612',
-        tabBarSelectedButtonColor: '#784532',
-        tabBarBackgroundColor: '#000000',
-        initialTabIndex: 1
-    },
-    appStyle: {
-        orientation: 'portrait', // Sets a specific orientation to the entire app. Default: 'auto'. Supported values: 'auto', 'landscape', 'portrait'
-        bottomTabBadgeTextColor: '#000000', // Optional, change badge text color. Android only
-        bottomTabBadgeBackgroundColor: '#985667', // Optional, change badge background color. Android only
-        backButtonImage: require('./img/select.png'), // Change the back button default arrow image with provided image. iOS only
-        hideBackButtonTitle: true // Hide back button title. Default is false. If `backButtonTitle` provided so it will take into account and the `backButtonTitle` value will show. iOS only
-    },
-    animationType: 'fade'
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: 'navigation.FirstTabScreen',
+        title:'home'
+        // topTabs: [
+        //   {
+        //     screenId: 'navigation.SecondTabScreen',
+        //     icon: require('./img/select.png')
+        //   },
+        //   {
+        //     screenId: 'navigation.PushedScreen',
+        //     icon: require('./img/unselect.png')
+        //   }
+        // ]
+    }
 });
+
+
+// Navigation.startTabBasedApp({
+//     tabs: [
+//         {
+            
+//             screen: 'navigation.FirstTabScreen', // this is a registered name for a screen
+//             icon: require('./img/select.png'),
+//             selectedIcon: require('./img/unselect.png'), // iOS only
+//             title: 'Screen One',
+//             titleImage: require('./img/select.png'),
+//             navigatorStyle: {
+
+//             },
+//             navigatorButtons: {}
+//         },
+//         {
+           
+//             screen: 'navigation.SecondTabScreen',
+//             icon: require('./img/select.png'),
+//             selectedIcon: require('./img/unselect.png'), // iOS only
+//             title: 'Screen Two'
+//         }
+//     ],
+//     tabsStyle: {
+//         tabBarButtonColor: '#985612',
+//         tabBarSelectedButtonColor: '#784532',
+//         tabBarBackgroundColor: '#000000',
+//         initialTabIndex: 1
+//     },
+//     appStyle: {
+//         orientation: 'portrait', // Sets a specific orientation to the entire app. Default: 'auto'. Supported values: 'auto', 'landscape', 'portrait'
+//         bottomTabBadgeTextColor: '#000000', // Optional, change badge text color. Android only
+//         bottomTabBadgeBackgroundColor: '#985667', // Optional, change badge background color. Android only
+//         backButtonImage: require('./img/select.png'), // Change the back button default arrow image with provided image. iOS only
+//         hideBackButtonTitle: true // Hide back button title. Default is false. If `backButtonTitle` provided so it will take into account and the `backButtonTitle` value will show. iOS only
+//     },
+//     animationType: 'fade'
+// });
